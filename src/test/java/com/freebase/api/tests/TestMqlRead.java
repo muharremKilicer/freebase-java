d185df3fb6fcb2d1d4df12d553dfe28a6f0cd630
stefano.mazzocchi <stefano.mazzocchi@ffe5e202-e998-11de-88ac-396b0f65d9f6>
2009-12-15 18:16:21 +0000

    initial commit (warning code is not tested yet)

---
+++ src/test/java/com/freebase/api/tests/TestMqlRead.java
@@ -0,0 +1,13 @@
+package com.freebase.api.tests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestMqlRead {
+
+    @Test public void test() {
+        assertTrue(true);
+    }
+}
+
