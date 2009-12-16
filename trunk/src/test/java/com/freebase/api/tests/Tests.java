e2a9eea8815c64f1723268bb84679007e86d0c19
stefano.mazzocchi <stefano.mazzocchi@ffe5e202-e998-11de-88ac-396b0f65d9f6>
2009-12-16 02:03:57 +0000

    improved usability for the contruction of JSON objects

--- trunk/src/test/java/com/freebase/api/tests/Tests.java
+++ trunk/src/test/java/com/freebase/api/tests/Tests.java
@@ -2,36 +2,19 @@ package com.freebase.api.tests;
 
 import static org.junit.Assert.assertTrue;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.freebase.api.Freebase;
 import com.freebase.json.JSON;
 
 public class Tests {
-
-    String id_query = "{ 'id' : null, 'limit' : 1 }";
-    
-    Logger logger = LoggerFactory.getLogger(Tests.class);
-    
-    public String normalize(String json) {
-        return json.replace("'", "\"");
-    }
-    
-    @Test public void testJSONParser() throws ParseException {
-        JSONParser parser = new JSONParser();
-        Object json = parser.parse(normalize(id_query));
-        assertTrue(json instanceof JSONObject);
-    }
-    
+        
     @Test public void testMQLRead() {
         Freebase freebase = Freebase.getFreebase();
-        JSON response = freebase.mqlread(normalize(id_query));
+        JSON query = JSON.o("id",null,"limit",1);
+        JSON response = freebase.mqlread(query);
         assertTrue("/user/root".equals(response.get("result").get("id").string()));
     }
+    
 }
 
