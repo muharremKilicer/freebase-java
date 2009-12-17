da9746ee42e1e810f6ad72e691211831e682796f
stefano.mazzocchi <stefano.mazzocchi@ffe5e202-e998-11de-88ac-396b0f65d9f6>
2009-12-17 00:29:50 +0000

    more tests for the JSON wrapper

--- src/test/java/com/freebase/json/tests/Tests.java
+++ src/test/java/com/freebase/json/tests/Tests.java
@@ -24,16 +24,49 @@ public class Tests {
     }
     
     @Test public void testJSON1() throws ParseException {
+        // make the json object by using the o/a notation
         JSON j1 = JSON.o("id",null,"limit",1);
+
+        // make the json object parsing the javascript syntax from a string 
         JSONParser parser = new JSONParser();
         JSON j2 = new JSON(parser.parse(query1));
+
+        // make sure they serialize the same
         assertTrue(j1.toString().equals(j2.toString()));
     }
     
     @Test public void testJSON2() throws ParseException {
+        // make the json object by using the o/a notation
         JSON j1 = a(o("a",1,"b",true,"c",null,"d",o("a1",a("blah"))));
+        
+        // make the json object parsing the javascript syntax from a string 
         JSONParser parser = new JSONParser();
         JSON j2 = new JSON(parser.parse(query2));
+
+        // make sure they serialize the same
+        assertTrue(j1.toString().equals(j2.toString()));
+    }
+
+    @Test public void testJSON3() throws ParseException {
+        // make the json object by using the o/a notation
+        JSON j1 = a()._(o()._("a",1)._("b",true)._("c",null)._("d",o()._("a1",a()._("blah"))));
+
+        // make the json object parsing the javascript syntax from a string 
+        JSONParser parser = new JSONParser();
+        JSON j2 = new JSON(parser.parse(query2));
+
+        // make sure they serialize the same
+        assertTrue(j1.toString().equals(j2.toString()));
+    }
+
+    @Test public void testJSON4() throws ParseException {
+        // make the json object by using the o/a notation
+        JSON j1 = a(o("a",1,"b",true,"c",null,"d",o("a1",a("blah"))));
+
+        // make the json object by using the _ appenders
+        JSON j2 = a()._(o()._("a",1)._("b",true)._("c",null)._("d",o()._("a1",a()._("blah"))));
+
+        // make sure they serialize the same
         assertTrue(j1.toString().equals(j2.toString()));
     }
     
